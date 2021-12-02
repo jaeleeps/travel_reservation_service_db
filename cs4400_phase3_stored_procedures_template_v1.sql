c160cf13f0eab61c02bc7e1e775f707489be9629
jaeyoung-lee <jaeleeps@gmail.com>
2021-12-02 02:27:35 -0500

    init ID: 5b cancel_property_reservation

--- cs4400_phase3_stored_procedures_template_v1.sql
+++ cs4400_phase3_stored_procedures_template_v1.sql
@@ -209,18 +209,15 @@ delimiter ;
 /**
 This procedure allows customers to reserve an available property advertised by an owner  if (and
 only if) the following conditions are met:
-• The combination of property_name, owner_email, and customer_email should be
-unique in the system
-• The start date of the reservation should be in the future (use current date for
-comparison)
-• The guest has not already reserved a property that overlaps with the dates of this
-reservation
-• The available capacity for the property during the span of dates must be greater than or
-equal to i_num_guests during the span of dates provided
+• The combination of property_name, owner_email, and customer_email should be unique in the system
+• The start date of the reservation should be in the future (use current date for comparison)
+• The guest has not already reserved a property that overlaps with the dates of this reservation
+• The available capacity for the property during the span of dates must be greater than or equal to i_num_guests during the span of dates provided
 • Note: for simplicity, the available capacity of a property over a span of time will be
 defined as the capacity of the property minus the total number of guests staying at that
 property during that span of time
  */
+-- FIXME: what about "was_cancelled" reserve?
 drop procedure if exists reserve_property;
 delimiter //
 create procedure reserve_property(
@@ -235,6 +232,7 @@ create procedure reserve_property(
 sp_main:
 begin
     if
+#         • The combination of property_name, owner_email, and customer_email should be unique in the system            
             (not exists
                 (
                     select 1
@@ -245,26 +243,29 @@ begin
                 )
                 )
             and
+#             • The start date of the reservation should be in the future (use current date for comparison)
             (i_current_date < i_start_date)
             and
+#             • The guest has not already reserved a property that overlaps with the dates of this reservation
             (not exists
                 (
                     select 1
                     from Reserve as r
                     where r.Customer = i_customer_email
                       and (
-                            i_start_date  between r.Start_Date AND r.End_Date
-                            or i_end_date  between r.Start_Date AND r.End_Date
+                            i_start_date between r.Start_Date and r.End_Date
+                            or i_end_date between r.Start_Date and r.End_Date
                         )
                 )
                 )
             and
+#             • The available capacity for the property during the span of dates must be greater than or equal to i_num_guests during the span of dates provided
             (
                 select if(
                                        (
                                            select Capacity
                                            from Property p
-                                           WHERE p.Property_Name = i_property_name
+                                           where p.Property_Name = i_property_name
                                        )
                                        -
                                        (
@@ -273,10 +274,10 @@ begin
                                                     left join Property p
                                                               on p.Property_Name = r.Property_Name
                                                                   and p.Owner_Email = r.Owner_Email
-                                           WHERE r.Property_Name = i_property_name
-                                             AND (
-                                                   r.Start_Date between i_start_date AND i_end_date
-                                                   or r.End_Date between i_start_date AND i_end_date
+                                           where r.Property_Name = i_property_name
+                                             and (
+                                                   r.Start_Date between i_start_date and i_end_date
+                                                   or r.End_Date between i_start_date and i_end_date
                                                )
                                        ) >= i_num_guests,
                                        1,
@@ -294,6 +295,14 @@ delimiter ;
 
 -- ID: 5b
 -- Name: cancel_property_reservation
+/**
+This procedure allows a customer to cancel an existing property reservation if (and only if) the following conditions are met:
+• The customer must already have reserved this property
+• If the reservation is already cancelled, this procedure should do nothing
+• The date of the reservation must be at a date in the future (use the current date passed in for comparison)
+• To cancel a reservation, the was_cancelled attribute in the reserve table should be set to 1
+
+ */
 drop procedure if exists cancel_property_reservation;
 delimiter //
 create procedure cancel_property_reservation(
@@ -304,8 +313,17 @@ create procedure cancel_property_reservation(
 )
 sp_main:
 begin
-    -- TODO: Implement your solution here
-
+    update Reserve as r
+#         • To cancel a reservation, the was_cancelled attribute in the reserve table should be set to 1
+    set r.Was_Cancelled = 1
+#         • The customer must already have reserved this property
+    where r.Property_Name = i_property_name
+      and r.Owner_Email = i_owner_email
+      and r.Customer = i_customer_email
+#         • If the reservation is already cancelled, this procedure should do nothing
+      and r.Was_Cancelled = 0
+#         • The date of the reservation must be at a date in the future (use the current date passed in for comparison)
+      and r.Start_Date > i_current_date;
 end //
 delimiter ;
 
